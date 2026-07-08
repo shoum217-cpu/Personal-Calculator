b17bd5683150e994fb89478b6cb84c5de73efcbd
Shoumil Mandal <shoum217@gmail.com>
2026-07-08 14:27:12 +0530

    Minimized errors

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const buttons = document.querySelectorAll("button:not(#clear):not(#calculate)")
 buttons.forEach(button => {
     button.addEventListener('click', () => {
         ClickToDisplay(button.textContent)
+        // agar dataset.value nahi mila then go for textContent here.. or is used so either this or that
     })
 })
 
@@ -23,7 +24,8 @@ const calculate = document.getElementById("calculate")
 
 calculate.addEventListener('click', () => {
     try {
-        display.value = eval(display.value);
+        const expression = display.value.replaceAll('÷','/')
+        display.value = eval(expression);
     }
     catch {
         display.value = "Error";
